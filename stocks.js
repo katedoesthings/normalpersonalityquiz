b46c309de71dbc63f825c141236ac036be72173e
katedoesthings <ktolguin@gmail.com>
2025-01-26 01:47:09 -0500

    added support for multiple stocks

--- stocks.js
+++ stocks.js
@@ -1,4 +1,34 @@
 let curStockPrices = [0, 0, 0, 0, 0, 0, 0, 0, 0, -1];
+let theStocks = [
+    {
+        "name": "Old Pennyworth's Butterscotch",
+        "stockPrices": [0, 0, 0, 0, 0, 0, 0, 0, 0, -1],
+        "color": [255, 0, 0],
+        "curPrice": -1,
+        "element": "pennyworthPrice",
+    },
+    {
+        "name": "Literally Cloning Dinosaurs",
+        "stockPrices": [0, 0, 0, 0, 0, 0, 0, 0, 0, -1],
+        "color": [0, 102, 0],
+        "curPrice": -1,
+        "element": "dinoCloning",
+    },
+    {
+        "name": "Eggulon: The New, Improved Egg",
+        "stockPrices": [0, 0, 0, 0, 0, 0, 0, 0, 0, -1],
+        "color": [76, 0, 153],
+        "curPrice": -1,
+        "element": "eggulon",
+    },
+    {
+        "name": "ChimpCorp (Their President Is A Literal Chimpanzee)",
+        "stockPrices": [0, 0, 0, 0, 0, 0, 0, 0, 0, -1],
+        "color": [0, 102, 204],
+        "curPrice": -1,
+        "element": "chimpcorp",
+    },
+]
 let yourMoney = 1000;
 let yourStock = 0;
 let frameForMurderModifier = 1;
@@ -9,49 +39,86 @@ function doTheLine(newLine) {
     clearCanvas();
     const c = document.getElementById("myCanvas");
     const ctx = c.getContext("2d");
+
     var interval = 30;
-    var minCeiled = Math.ceil(0);
-    var maxFloored = Math.floor(150);
-    var theStockPrice = Math.floor((Math.random() * (maxFloored - minCeiled + 1) + minCeiled) * frameForMurderModifier * unrealisticOutlookModifier);
-    if (curStockPrices[curStockPrices.length - 1] > 0) {
-        var x = 0;
-        while (x < 5 || theStockPrice < curStockPrices[curStockPrices.length - 1] - 40 || theStockPrice > curStockPrices[curStockPrices.length - 1] + 40) {
-            theStockPrice = Math.floor((Math.random() * (maxFloored - minCeiled + 1) + minCeiled) * frameForMurderModifier * unrealisticOutlookModifier);
-            x++;
+
+    for (var j in theStocks) {
+        if (theStocks[j].name == "Old Pennyworth's Butterscotch") {
+            theStocks[j].curPrice = generateStockPrice(1, 150, theStocks[j].curPrice, 40)
+        }
+        if (theStocks[j].name == "Literally Cloning Dinosaurs") {
+            theStocks[j].curPrice = generateStockPrice(75, 150, theStocks[j].curPrice, 40)
         }
     }
-    
+
+
     var whereAreWe = 0;
     if (newLine == true) {
-        ctx.beginPath();
-        ctx.moveTo(0, theStockPrice);
-        for (var key in curStockPrices) {
-            curStockPrices[key] = theStockPrice;
-            theStockPrice = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
-            theStockPoint = 150 - theStockPrice;
-            ctx.lineTo(whereAreWe + interval, theStockPoint);
-            ctx.stroke();
-            whereAreWe += interval;
+        for (var key in theStocks) {
+            whereAreWe = 0;
+            ctx.beginPath();
+            ctx.moveTo(0, theStocks[key].curPrice);
+            if (theStocks[key].name == "Old Pennyworth's Butterscotch") {
+                for (var k in theStocks[key].stockPrices) {
+                    theStocks[key].stockPrices[k] = theStockPrice;
+                    theStockPrice = generateStockPrice(1, 150, -1, 40);
+                    theStockPoint = 200 - theStockPrice;
+                    ctx.lineTo(whereAreWe + interval, theStockPoint);
+                    ctx.strokeStyle = "rgb(" + theStocks[key].color[0] + " " + theStocks[key].color[1] + " " + theStocks[key].color[2] + ")";
+                    ctx.stroke();
+                    whereAreWe += interval;
+                }
+            }
+            if (theStocks[key].name == "Literally Cloning Dinosaurs") {
+                for (var k in theStocks[key].stockPrices) {
+                    theStocks[key].stockPrices[k] = theStockPrice;
+                    theStockPrice = generateStockPrice(1, 150, -1, 40);
+                    theStockPoint = 200 - theStockPrice;
+                    ctx.lineTo(whereAreWe + interval, theStockPoint);
+                    ctx.strokeStyle = "rgb(" + theStocks[key].color[0] + " " + theStocks[key].color[1] + " " + theStocks[key].color[2] + ")";
+                    ctx.stroke();
+                    whereAreWe += interval;
+                }
+            }
         }
     }
     else {
-        ctx.beginPath();
-        ctx.moveTo(0, 150 - curStockPrices[0]);
-        for (i = 0; i < curStockPrices.length; i++) {
-            if (i == curStockPrices.length - 1) {
-                curStockPrices[i] = theStockPrice;
+        for (var y in theStocks) {
+            whereAreWe = 0;
+            ctx.beginPath();
+                ctx.moveTo(0, 200 - theStocks[y].curPrice);
+                for (i = 0; i <= theStocks[y].stockPrices.length - 1; i++) {
+                    if (i == theStocks[y].stockPrices.length - 1) {
+                        theStocks[y].stockPrices[i] = theStocks[y].curPrice;
+                    }
+                    else {
+                        theStocks[y].stockPrices[i] = theStocks[y].stockPrices[i + 1];
+                    }
+                    ctx.lineTo(whereAreWe + interval, 200 - theStocks[y].stockPrices[i]);
+                    ctx.strokeStyle = "rgb(" + theStocks[y].color[0] + " " + theStocks[y].color[1] + " " + theStocks[y].color[2] + ")";
+                    ctx.stroke();
+                    whereAreWe += interval;
             }
-            else {
-                curStockPrices[i] = curStockPrices[i + 1];
-            }
-            ctx.lineTo(whereAreWe + interval, 150 - curStockPrices[i]);
-            ctx.stroke();
-            whereAreWe += interval;
+            document.getElementById(theStocks[y].element).innerHTML = theStocks[y].name + " Price: $" + theStocks[y].stockPrices[theStocks[y].stockPrices.length - 1];
+
         }
     }
-    document.getElementById('yuh').innerHTML = "Stock Price: $" + curStockPrices[curStockPrices.length-1];
 }
 
+
+function generateStockPrice(min, max, compare=-1, threshold, modifier1=1, modifier2=1, modifier3=1, modifier4=1, modifier5=1) {
+    var minCeiled = Math.ceil(min);
+    var maxFloored = Math.floor(max);
+    var theStockPrice = Math.floor((Math.random() * (maxFloored - minCeiled + 1) + minCeiled) * modifier1 * modifier2 * modifier3 * modifier4 * modifier5);
+    if (compare > 0) {
+        var x = 0;
+        while (x < 5 || theStockPrice < compare - threshold || theStockPrice > compare + threshold) {
+            theStockPrice = Math.floor((Math.random() * (maxFloored - minCeiled + 1) + minCeiled) * modifier1 * modifier2 * modifier3 * modifier4 * modifier5);
+            x++;
+        }
+    }
+    return theStockPrice;
+}
 function clearCanvas() {
     const c = document.getElementById("myCanvas");
     const ctx = c.getContext("2d");
@@ -80,7 +147,7 @@ function updateYourMoney() {
     document.getElementById('yourMoney').innerHTML = "$" + yourMoney;
 }
 
-function setModifier(cost, ending, modifier, modifierName) {
+function setModifier(cost, ending, modifier, modifierName, time) {
     if (ending == true) {
         if (modifierName == "framedForMurderModifier") {
             frameForMurderModifier = modifier;
@@ -104,7 +171,7 @@ function setModifier(cost, ending, modifier, modifierName) {
     }
 
     updateYourMoney();
-    setTimeout(() => setModifier(0, true, 1, modifierName), 6000);
+    setTimeout(() => setModifier(0, true, 1, modifierName), time);
 }
 
 /*
