605873fa183c4c16f4c597fee9d59f66c92334fa
katedoesthings <ktolguin@gmail.com>
2025-03-29 11:07:41 -0400

    More stock things lmao forgot about that plus populating cooking recipes

--- cooking-info.js
+++ cooking-info.js
@@ -2,26 +2,84 @@
 let theRecipes = [
     {
         "name": "Southwestern Scramble",
-        "ingredients": ["Ground Chicken", "Kale", "Lemon Juice", "Salt", "Frozen Corn", "Smoked Paprika", "Adobo", "Onion Powder", "Garlic Powder", "Red Bell Pepper", "Potato", "Tomatillo Salsa"],
-        "numIngredients": [1,null,2,3,1,6,9,2, null, null, null, null],
-        "recipe": "Fuckin I dunno"
+        "ingredients": ["Ground Chicken", "Kale", "Lemon Juice", "Soy Sauce", "Salt", "Frozen Corn", "Smoked Paprika", "Adobo", "Onion Powder", "Garlic Powder", "Red Bell Pepper", "Gold Potato", "Tomatillo Salsa"],
+        "numIngredients": [1, 1, 2, 2, null, 1, null, null, null, null, 2, 3, 1],
+        "units": ["lb", "large bag", "tbsp", "tbsp", null, "large bag", null, null, null, null, null, "medium", "jar"],
+        "recipe": "1. Preheat the oven to 425F and slice the bell pepper into medium strips (about half the width of your finger). Place the bell pepper on a baking sheet lined with parchment paper or tinfoil and drizzle heavily with olive oil. Season with the adobo, onion powder, garlic powder, and smoked paprika and bake until tender, about 15 mins. <br><br>" +
+            "2. Prepare the frozen corn by adding it to a pan with a neutral oil and seasoning it with adobo, onion powder, garlic powder, and smoked paprika. Stir frequently once the corn is defrosted to prevent burning, and cook until the corn shows signs of crispiness.<br><br>" +
+            "3. Prepare the potato by dicing it and addint it to a pan with a neutral oil and seasoning it with adobo, onion powder, garlic powder, and smoked paprika. Stir infrequently, enough to let them crisp up on the pan but not so infrequently that it burns. Cook until the edges are browned and a bit crispy.<br><br>" +
+            "4. Prepare the ground chicken by adding it to a pan with a neutral oil and seasoning it with adobo, onion powder, garlic powder, and smoked paprika. Fry on medium until cooked through.<br><br>" +
+            "5. Prepare the kale by removing the stems of the kale and adding the leaves to a bowl. Once de-stemmed, add lemon juice, salt, and soy sauce. Combine until the kale is coated.<br><br>" +
+            "6. Assemble your bowl by adding corn, potato, ground chicken, and kale, and topping with tomatillo salsa. Store ingredients separately."
     },
     {
-        "name": "Thai-Inspired Salad Bowl",
-        "ingredients": ["Ground Beef", "Red Onion", "Red Cabbage", "Peanuts", "Shredded Carrots", "Mango", "Cilantro", "Bean Sprouts"],
-        "numIngredients": [1, null, 2, 3, 1, 6, 9, 2],
-        "recipe": "Fuckin I dunno, recommend serve with thai peanut sauce"
+        "name": "Thai-Inspired Noodle Bowl",
+        "ingredients": ["Ground Beef", "Red Onion", "Red Cabbage", "Peanuts", "Shredded Carrots", "Mango", "Cilantro", "Bean Sprouts", "Rice Noodles", "Soy Sauce", "Garlic", "Onion Powder"],
+        "numIngredients": [1, 1, .5, .5, 1, 1, null, 1, null, null, null, null],
+        "units": ["lb", null, "head", "cup", "bag", null, null, "bag", null, null, null, null, null],
+        "recipe": "1. Prepare the ground beef by adding it to a pan and seasoning it with soy sauce, garlic, and onion powder. Stir infrequently, enough to let it crisp up on the pan but not so infrequently that it burns. Fry on medium until cooked through.<br><br>" +
+            "2. Prepare the rice noodles according to their packaging. <br><br>" +
+            "3. Dice the red onion, mango, and cabbage. The red onion should be a small dice, the mango should be a medium dice, and the cabbage should be a large dice. Crush the peanuts and chop the cilantro, including the stems. <br><br>" +
+            "4. Assemble the bowl by adding noodles, red onion, red cabbage, shredded carrots, mango, and bean sprouts, topping with cilantro and crushed peanuts. Store ingredients separately.<br><br>" +
+            "Serve with preferred dressing. I highly recommend the peanut sauce also found on this recipe list."
     },
     {
-        "name": "splimpo",
-        "ingredients": ["plimborrrtrrr", "grug", "pemberton farms", "tomato"],
-        "numIngredients": [1, null, 2, 3, 1, 6, 9, 2],
-        "recipe": "Fuckin I dunno, recommend serve with thai peanut sauce"
+        "name": "Thai Peanut Sauce",
+        "ingredients": ["Peanut Butter", "Soy Sauce", "Rice Vinegar", "Brown Sugar", "Chili Garlic Sauce", "Lime Juice", "Garlic", "Ginger", "Water"],
+        "numIngredients": [.5, 2, 1, 2, 2, 1, 3, 1, 4],
+        "units": ["cup", "tbsp", "tbsp", "tbsp", "tsp", "tbsp", "cloves", "tbsp", "tbsp"],
+        "recipe": "From Fox and Briar <br><br>" +
+            "1. Combine all ingredients except water and stir. <br><br>" +
+            "2. Add water, 1 tablespoon at a time, until desired consistency is reached. <br><br>" +
+            "Note: The chili garlic sauce referred to in the ingredients list is called that verbatim in the grocery store. It's in a little jar in the Asian food aisle. " +
+            "Also, it's best if you can use fresh ginger or garlic, but it's also fine to substitute ginger or garlic powder. The flavor just won't be as strong."
+    },
+    {
+        "name": "Potato Soup",
+        "ingredients": ["White Onion", "Salt", "Garlic", "Wine Vinegar", "Vegetable Broth", "Gold Potato", "Cooked White Beans", "Dijon Mustard", "Lemon Juice", "Smoked Paprika", "Black Pepper", "Ground Chicken", "Garlic Powder", "Onion Powder"],
+        "numIngredients": [1, null, 7, 1, 4, 5, 1.5, 1, 1, 1, null, 1],
+        "units": [null, null, "cloves", "tbsp", "cup", "medium", "cup", "tbsp", "tbsp", "tsp", null, "lb"],
+        "recipe": "Largely from Love and Lemons <br><br>" + 
+            "1. Roughly dice the onion. Saute the onion with a neutral oil and some salt and pepper until soft. <br><br>" +
+            "2. Roughly chop the garlic and add to the onion. Saute it for about 2 more minutes until garlic is fragrant, stirring frequently. <br><br>" +
+            "3. Add wine vinegar (red or white is fine), broth, potatoes, and white beans. Bring to a boil, then reduce the heat and simmer for 30 minutes or until potatoes are soft, whichever happens last. <br><br>" +
+            "4. While the soup is cooking, cook the ground chicken in a separate pan with a neutral oil, garlic powder, salt, pepper, and onion powder. When finished, set aside. <br><br>" +
+            "5. Add mustard, lemon juice, and paprika. Blend in a blender or with an immersion blender. If you want to be really fancy you can blend half of it and mash the chunks in the other half, then recombine depending on your texture preference.<br><br>" +
+            "6. Season to taste with salt and pepper. Top with your desired amount of the ground chicken for protein, and store soup and ground chicken separately. I recommend enjoying it with a nice crusty bread as well!"
+    },
+    {
+        "name": "Cilantro Lime Crema",
+        "ingredients": ["Sour Cream", "Mayo", "Lime Juice", "Garlic", "Salt", "Onion Powder", "Cilantro"],
+        "numIngredients": [1, 2, 1, 1, 1, .5, 1],
+        "units": ["cup", "tbsp", "tbsp", "clove", "tsp", "tsp", "cup"],
+        "recipe": "From Love and Lemons. You can also substitute with dairy free sour cream or yogurt if you're like me and can't do the dairy thing. <br><br>" +
+        "1. Combine that jazz in a food processor until creamy! Easy peasy. Sauce should be smooth but green flecks of cilantro should still be visible."
+    },
+    {
+        "name": "Mango Mambo Turkey Sandwich",
+        "ingredients": ["Sourdough", "Turkey", "Tomato", "Arugula", "Sharp Cheddar", "Honey Mustard", "Mango Chutney", "Butter"],
+        "numIngredients": [null, null, 1, 1, null, null, null, null],
+        "units": [null, "Deli", null, "Block", null, null, null, null],
+        "recipe": "Honey mustard should be the zestiest you can find. Mango chutney can also be replaced with some kind of mango or peach jam if the people at the grocery store look at you weird when you ask where the chutney is. Which has definitely never happened to me once. <br><br>" +
+            "1. Slice the tomato and sharp cheddar cheese. <br><br>" +
+            "2. Assemble one half of the sandwich with the following, in this order: Turkey, Cheddar, Arugula, Tomato. <br><br>" +
+            "3. Slather the other half with mango chutney and honey mustard in a 1:1 ratio. <br><br>" +
+            "4. Heat a pan and grease it with butter. Place the sandwich in the pan. When one side is crispy, take the sandwich off of the pan, re-grease the pan with more butter, then return the sandwich to the pan, crispy side up. <br><br>" +
+            "5. Once both sides are crispy to your liking, remove from heat and enjoy! <br><br>" +
+            "Also, you don't need to use butter, but that's my preferred grease for making sandwiches crispy."
+    },
+    {
+        "name": "",
+        "ingredients": [],
+        "numIngredients": [],
+        "units": [],
+        "recipe": ""
     },
     {
         "name": "Devilish Eggs",
         "ingredients": ["plimborrrtrrr", "gaaaaaaag", "pemberton farms", "tomato"],
         "numIngredients": [1, null, 2, 3, 1, 6, 9, 2],
+        "units": [],
         "recipe": "Fuckin I dunno, recommend serve with thai peanut sauce"
     }
 ]
@@ -83,7 +141,7 @@ function populateIngredients(theKey) {
 
     for (var ingredient in theRecipes[theKey].ingredients)
     {
-        createCheckboxes(theRecipes[theKey].ingredients[ingredient], theRecipes[theKey].numIngredients[ingredient], false, 'ingredientslist');
+        createCheckboxes(theRecipes[theKey].ingredients[ingredient], theRecipes[theKey].numIngredients[ingredient], false, 'ingredientslist', theRecipes[theKey].units[ingredient]);
         isHoldover = false;
         for (var key in holdoverIngredients) {
             if (holdoverIngredients[key] == theRecipes[theKey].ingredients[ingredient]) {
@@ -109,7 +167,7 @@ function populateIngredients(theKey) {
     document.getElementById('ingredientslist').appendChild(addToListBtn);
 }
 
-function createCheckboxes(theID, theNum, theValue, theOverallID) {
+function createCheckboxes(theID, theNum, theValue, theOverallID, theUnit) {
     var checkbox = document.createElement('input');
     checkbox.type = "checkbox";
     checkbox.id = theID;
@@ -119,14 +177,24 @@ function createCheckboxes(theID, theNum, theValue, theOverallID) {
     var label = document.createElement('label')
     label.htmlFor = theID;
     label.id = theID + "label";
-    label.appendChild(document.createTextNode(" " + theID));
+    if (theUnit != null) {
+        label.appendChild(document.createTextNode(" " + theUnit + " " + theID));
+    }
+    else {
+        label.appendChild(document.createTextNode(" " + theID));
+    }
 
     if (theOverallID == "groceryList") {
 
         var numberOfIngredient = document.createElement('input');
         numberOfIngredient.type = "number";
         numberOfIngredient.min = 0;
-        numberOfIngredient.value = theNum;
+        if (theNum > 0) {
+            numberOfIngredient.value = theNum;
+        }
+        else {
+            numberOfIngredient.value = 1;
+        }
         numberOfIngredient.id = theID + 'number';
     }
 
@@ -162,7 +230,7 @@ function removeSelectedItems(theID) {
     }
 
     for (var key in holdoverIngredients) {
-        createCheckboxes(holdoverIngredients[key], holdoverIngredientsNum[key], false, theID);
+        createCheckboxes(holdoverIngredients[key], holdoverIngredientsNum[key], false, theID, null);
     }
 
     populateIngredients(curRecipeKey);
@@ -256,7 +324,7 @@ function onAddToListBtnClicked() {
                 theresGroceries = true;
             }
             if (document.getElementById('ingredientslist').children[key].checked == true && !isHoldover) {
-                createCheckboxes(document.getElementById('ingredientslist').children[key].id, Number(document.getElementById(document.getElementById('ingredientslist').children[key].id + "number").innerHTML), false, 'groceryList');
+                createCheckboxes(document.getElementById('ingredientslist').children[key].id, Number(document.getElementById(document.getElementById('ingredientslist').children[key].id + "number").innerHTML), false, 'groceryList', null);
             }
         }
     }
--- stocks.js
+++ stocks.js
@@ -2,42 +2,88 @@ let curStockPrices = [0, 0, 0, 0, 0, 0, 0, 0, 0, -1];
 let theStocks = [
     {
         "name": "Old Pennyworth's Butterscotch",
+        "abbrv": "PWB",
         "stockPrices": [0, 0, 0, 0, 0, 0, 0, 0, 0, -1],
+        "margin": 30,
+        "fluxchance": 0,
+        "fluxthreshold": 60,
+        "bouncebackthreshold": 80,
+        "bouncebackmargin": 20,
+        "bounceback": -3,
         "color": [255, 0, 0],
         "curPrice": -1,
         "element": "pennyworthPrice",
+        "numYouHave": 0,
+        "modifiers": 1,
     },
     {
         "name": "Literally Cloning Dinosaurs",
+        "abbrv": "DINO",
         "stockPrices": [0, 0, 0, 0, 0, 0, 0, 0, 0, -1],
         "color": [0, 102, 0],
+        "fluxchance": 3,
+        "fluxthreshold": -100,
+        "bouncebackthreshold": 140,
+        "bouncebackmargin": 20,
+        "bounceback": 5,
+        "margin": 10,
         "curPrice": -1,
         "element": "dinoCloning",
+        "numYouHave": 0,
+        "modifiers": 1,
     },
     {
         "name": "Eggulon: The New, Improved Egg",
+        "abbrv": "EGG",
         "stockPrices": [0, 0, 0, 0, 0, 0, 0, 0, 0, -1],
         "color": [76, 0, 153],
+        "margin": 10,
+        "fluxchance": 3,
+        "fluxthreshold": 150,
+        "bouncebackthreshold": 20,
+        "bouncebackmargin": 20,
+        "bounceback": -5,
         "curPrice": -1,
         "element": "eggulon",
+        "numYouHave": 0,
+        "modifiers": 1,
     },
     {
         "name": "ChimpCorp (Their President Is A Literal Chimpanzee)",
+        "abbrv": "CHMP",
         "stockPrices": [0, 0, 0, 0, 0, 0, 0, 0, 0, -1],
-        "color": [0, 102, 204],
+        "color": [204, 204, 0],
+        "margin": 100,
+        "fluxchance": 0,
+        "fluxthreshold": 60,
+        "bouncebackthreshold": 0,
+        "bouncebackmargin": 0,
+        "bounceback": 0,
         "curPrice": -1,
         "element": "chimpcorp",
+        "numYouHave": 0,
+        "modifiers": 1,
     },
-
     {
-    "name": "It's Just The Moon",
-    "stockPrices": [0, 0, 0, 0, 0, 0, 0, 0, 0, -1],
-    "color": [0, 102, 204],
-    "curPrice": -1,
-    "element": "themoon",
+        "name": "It's Just The Moon",
+        "abbrv": "MOON",
+        "stockPrices": [0, 0, 0, 0, 0, 0, 0, 0, 0, -1],
+        "color": [30, 144, 255],
+        "margin": 10,
+        "fluxchance": 0,
+        "fluxthreshold": 0,
+        "bouncebackthreshold": 170,
+        "bouncebackmargin": 10,
+        "bounceback": 5,
+        "curPrice": -1,
+        "element": "themoon",
+        "numYouHave": 0,
+        "modifiers": 1,
     },
 ]
-let yourMoney = 1000;
+
+let curMax = 200;
+let yourMoney = 10000;
 let yourStock = 0;
 let frameForMurderModifier = 1;
 let unrealisticOutlookModifier = 1;
@@ -50,22 +96,78 @@ function doTheLine(newLine) {
 
     var interval = 30;
 
+
     for (var j in theStocks) {
-        if (theStocks[j].name == "Old Pennyworth's Butterscotch") {
-            theStocks[j].curPrice = generateStockPrice(1, 150, theStocks[j].curPrice, 40)
+        var generateMin;
+        var generateMax;
+
+        if ((theStocks[j].curPrice - theStocks[j].margin) > 0) {
+            generateMin = theStocks[j].curPrice - theStocks[j].margin;
         }
-        if (theStocks[j].name == "Literally Cloning Dinosaurs") {
-            theStocks[j].curPrice = generateStockPrice(75, 150, theStocks[j].curPrice, 40)
+        else {
+            generateMin = 1;
         }
+
+        /*if (generateMin + theStocks[j].bounceback > 0 && (generateMin < theStocks[j].bouncebackthreshold - theStocks[j].bouncebackmargin)) {
+            generateMin += theStocks[j].bounceback;
+        }*/
+
+        if ((theStocks[j].curPrice + theStocks[j].margin) < curMax) {
+            generateMax = theStocks[j].curPrice + theStocks[j].margin;
+        }
+        else {
+            generateMax = curMax;
+        }
+        /*
+        if (generateMax + theStocks[j].bounceback < curMax && (generateMax < theStocks[j].bouncebackthreshold - theStocks[j].bouncebackmargin)) {
+            generateMax += theStocks[j].bounceback;
+        }
+        */
+        if (theStocks[j].bounceback > 0) {
+            if ((generateMax < theStocks[j].bouncebackthreshold - theStocks[j].bouncebackmargin)) {
+                generateMax += theStocks[j].bounceback;
+                generateMin += theStocks[j].bounceback;
+            }
+            else if (generateMax > theStocks[j].bouncebackthreshold - theStocks[j].bouncebackmargin && theStocks[j].abbrv == "MOON") {
+                if (theStocks[j].bouncebackthreshold == 170) {
+                    theStocks[j].bouncebackthreshold = 10;
+                    theStocks[j].bounceback = -5;
+                }
+            }
+        }
+        else {
+            if ((generateMin > theStocks[j].bouncebackthreshold + theStocks[j].bouncebackmargin)) {
+                generateMax += theStocks[j].bounceback;
+                generateMin += theStocks[j].bounceback;
+            }
+            else if (generateMin < theStocks[j].bouncebackthreshold + theStocks[j].bouncebackmargin && theStocks[j].abbrv == "MOON") {
+                if (theStocks[j].bouncebackthreshold == 10) {
+                    theStocks[j].bouncebackthreshold = 170;
+                    theStocks[j].bounceback = 5;
+                }
+            }
+        }
+
+        theStocks[j].curPrice = generateStockPrice(generateMin, generateMax, theStocks[j].fluxchance, theStocks[j].fluxthreshold, theStocks[j].modifiers);
+
     }
 
 
     var whereAreWe = 0;
     if (newLine == true) {
+        for (var j in theStocks) {
+            if (theStocks[j].name == "Old Pennyworth's Butterscotch") {
+                theStocks[j].curPrice = generateStockPrice(1, 75, theStocks[j].curPrice, 40)
+            }
+            if (theStocks[j].name == "Literally Cloning Dinosaurs") {
+                theStocks[j].curPrice = generateStockPrice(75, 150, theStocks[j].curPrice, 40)
+            }
+        }
+        /*
         for (var key in theStocks) {
             whereAreWe = 0;
             ctx.beginPath();
-            ctx.moveTo(0, theStocks[key].curPrice);
+            ctx.moveTo(0, 200 - theStocks[key].stockPrices[0]);
             if (theStocks[key].name == "Old Pennyworth's Butterscotch") {
                 for (var k in theStocks[key].stockPrices) {
                     theStocks[key].stockPrices[k] = theStockPrice;
@@ -89,12 +191,13 @@ function doTheLine(newLine) {
                 }
             }
         }
+        */
     }
     else {
         for (var y in theStocks) {
             whereAreWe = 0;
             ctx.beginPath();
-                ctx.moveTo(0, 200 - theStocks[y].curPrice);
+            ctx.moveTo(0, 200 - theStocks[y].stockPrices[0]);
                 for (i = 0; i <= theStocks[y].stockPrices.length - 1; i++) {
                     if (i == theStocks[y].stockPrices.length - 1) {
                         theStocks[y].stockPrices[i] = theStocks[y].curPrice;
@@ -103,28 +206,61 @@ function doTheLine(newLine) {
                         theStocks[y].stockPrices[i] = theStocks[y].stockPrices[i + 1];
                     }
                     ctx.lineTo(whereAreWe + interval, 200 - theStocks[y].stockPrices[i]);
+                    ctx.lineWidth = 3;
                     ctx.strokeStyle = "rgb(" + theStocks[y].color[0] + " " + theStocks[y].color[1] + " " + theStocks[y].color[2] + ")";
                     ctx.stroke();
                     whereAreWe += interval;
             }
-            document.getElementById(theStocks[y].element).innerHTML = theStocks[y].name + " Price: $" + theStocks[y].stockPrices[theStocks[y].stockPrices.length - 1];
+            document.getElementById(theStocks[y].element).innerHTML = theStocks[y].name + " (" + theStocks[y].abbrv + ") " + "Price: $" + theStocks[y].stockPrices[theStocks[y].stockPrices.length - 1];
 
         }
     }
 }
 
 
-function generateStockPrice(min, max, compare=-1, threshold, modifier1=1, modifier2=1, modifier3=1, modifier4=1, modifier5=1) {
-    var minCeiled = Math.ceil(min);
-    var maxFloored = Math.floor(max);
+function generateStockPrice(min, max, fluxchance, fluxthreshold, modifier1=1, modifier2=1, modifier3=1, modifier4=1, modifier5=1) {
+    var minCeiled;
+    var maxFloored;
+    minCeiled = Math.ceil(min);
+    maxFloored = Math.floor(max);
+    var chaoschaos = Math.floor(Math.random() * 100) + 1;
+    if (chaoschaos < fluxchance) {
+        if (min + fluxthreshold <= 0) {
+            minCeiled = Math.ceil(1);
+        }
+        else if (min + fluxthreshold >= curMax) {
+            minCeiled = Math.ceil(curMax - 1);
+        }
+        else {
+            minCeiled = Math.ceil(min + fluxthreshold);
+        }
+
+        if (max + fluxthreshold <= 0) {
+            maxCeiled = Math.floor(2);
+        }
+        else if (max + fluxthreshold >= curMax) {
+            maxCeiled = Math.floor(curMax);
+        }
+        else {
+            maxCeiled = Math.floor(max + fluxthreshold + 1);
+        }
+    }
+
     var theStockPrice = Math.floor((Math.random() * (maxFloored - minCeiled + 1) + minCeiled) * modifier1 * modifier2 * modifier3 * modifier4 * modifier5);
-    if (compare > 0) {
+    /*if (compare > 0) {
         var x = 0;
         while (x < 5 || theStockPrice < compare - threshold || theStockPrice > compare + threshold) {
             theStockPrice = Math.floor((Math.random() * (maxFloored - minCeiled + 1) + minCeiled) * modifier1 * modifier2 * modifier3 * modifier4 * modifier5);
             x++;
         }
+    } */
+    if (theStockPrice <= 0) {
+        theStockPrice = 1;
     }
+    if (theStockPrice >= curMax) {
+        theStockPrice = curMax;
+    }
+
     return theStockPrice;
 }
 function clearCanvas() {
@@ -133,53 +269,63 @@ function clearCanvas() {
     ctx.clearRect(0, 0, c.width, c.height);
 }
 
-function buyStock() {
-    if (yourMoney >= curStockPrices[curStockPrices.length - 1]) {
-        yourMoney -= curStockPrices[curStockPrices.length - 1];
-        document.getElementById('yourMoney').innerHTML = "$" + yourMoney;
-        yourStock += 1;
-        document.getElementById('yourStock').innerHTML = "You have: " + yourStock + " stock";
-    }
+function buyStock(stockName) {
+    for (var key in theStocks)
+        if (theStocks[key].abbrv == stockName) {
+            if (yourMoney >= theStocks[key].stockPrices[theStocks[key].stockPrices.length - 1]) {
+                yourMoney -= theStocks[key].stockPrices[theStocks[key].stockPrices.length - 1];
+                document.getElementById('yourMoney').innerHTML = "$" + yourMoney;
+                theStocks[key].numYouHave += 1;
+                document.getElementById(stockName + "CurStock").innerHTML = "You have: " + theStocks[key].numYouHave + " " + stockName + " stock";
+            }
+        }
 }
 
-function sellStock() {
-    if (yourStock > 0) {
-        yourMoney += curStockPrices[curStockPrices.length - 1];
-        document.getElementById('yourMoney').innerHTML = "$" + yourMoney;
-        yourStock -= 1;
-        document.getElementById('yourStock').innerHTML = "You have: " + yourStock + " stock";
-    }
+function sellStock(stockName) {
+    for (var key in theStocks)
+        if (theStocks[key].abbrv == stockName) {
+            if (theStocks[key].numYouHave > 0) {
+                yourMoney += theStocks[key].stockPrices[theStocks[key].stockPrices.length - 1];
+                document.getElementById('yourMoney').innerHTML = "$" + yourMoney;
+                theStocks[key].numYouHave -= 1;
+                document.getElementById(stockName + "CurStock").innerHTML = "You have: " + theStocks[key].numYouHave + " " + stockName + " stock";
+            }
+        }
 }
 
 function updateYourMoney() {
     document.getElementById('yourMoney').innerHTML = "$" + yourMoney;
 }
 
-function setModifier(cost, ending, modifier, modifierName, time) {
-    if (ending == true) {
-        if (modifierName == "framedForMurderModifier") {
-            frameForMurderModifier = modifier;
-            document.getElementById('framedForMurder').innerHTML = "";
+function setModifier(stockName, cost, ending, modifier, modifierName, time) {
+    for (var key in theStocks)
+        if (theStocks[key].abbrv == stockName) {
+            if (ending == true) {
+                theStocks[key].modifiers = modifier;
+                document.getElementById('framedForMurder').innerHTML = "";
+                document.getElementById(stockName + 'UpButton').disabled = false;
+                document.getElementById(stockName + 'DownButton').disabled = false;
+            }
+            else if (ending != true && cost <= yourMoney) {
+                yourMoney -= cost;
+                theStocks[key].modifiers *= modifier;
+                document.getElementById('framedForMurder').innerHTML = stockName + " CEO committed a ghastly murder! Stock price down " + ((1.0 - modifier) * 100) + "%";
+                document.getElementById(stockName + 'UpButton').disabled = true;
+                document.getElementById(stockName + 'DownButton').disabled = true;
+                /*
+                if (modifierName == "framedForMurderModifier") {
+                    document.getElementById('framedForMurder').innerHTML = stockName + " CEO committed a ghastly murder! Stock price down " + ((1.0 - modifier) * 100) + "%";
+                }
+                if (modifierName == "unrealisticOutlookModifier") {
+                    document.getElementById('unrealisticOutlook').innerHTML = "This year " + stockName + " will make a bajillion dabloons! Stock price up " + ((modifier - 1.0) * 100) + "%";
+                }*/
+            }
         }
-        if (modifierName == "unrealisticOutlookModifier") {
-            unrealisticOutlookModifier = modifier;
-            document.getElementById('unrealisticOutlook').innerHTML = "";
-        }
-    }
-    else if (cost <= yourMoney) {
-        yourMoney -= cost;
-        if (modifierName == "framedForMurderModifier") {
-            frameForMurderModifier = modifier;
-            document.getElementById('framedForMurder').innerHTML = "CEO committed a ghastly murder! Stock price down " + ((1.0 - modifier) * 100) + "%";
-        }
-        if (modifierName == "unrealisticOutlookModifier") {
-            unrealisticOutlookModifier = modifier;
-            document.getElementById('unrealisticOutlook').innerHTML = "This year the company will make a bajillion dabloons! Stock price up " + ((modifier - 1.0) * 100) + "%";
-        }
-    }
 
     updateYourMoney();
-    setTimeout(() => setModifier(0, true, 1, modifierName), time);
+    if (!ending) {
+        setTimeout(() => setModifier(stockName, 0, true, 1, modifierName), time);
+    }
 }
 
 /*
